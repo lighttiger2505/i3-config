5588f7ec02991bac67444da3e16c617d1d27e989
toshikazu.ohashi <toshikazu.ohashi@iridge.jp>
2019-06-02 03:44:45 +0900

    Fix detect display script

--- i3/detect_displays.sh
+++ i3/detect_displays.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-display="HDMI2"
+display="HDMI1"
 st_prev=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/" | grep ${display})
 
 while :
@@ -11,7 +11,7 @@ do
         if [ "${st_next}" = "${display}" ]; then
             xrandr --output eDP1 --mode 1920x1080 --output ${display} --auto --above eDP1
         else
-            xrandr --output eDP1 --mode 1920x1080 --output HDMI2 --off
+            xrandr --output eDP1 --mode 1920x1080 --output ${display} --off
         fi
     fi
     sleep 1
